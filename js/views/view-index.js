1ae0e7d0c5a73cb382e27fb0a5f1356e0b313b2c
colaboy <49201650@qq.com>
2016-09-01 16:06:00 +0800

    格式调整

--- js/views/view-index.js
+++ js/views/view-index.js
@@ -1,52 +1,66 @@
 require(
 	['zepto','template','text!../templates/tpl-index.html','validate'],
 	function($,template,tplIndex,validate){
-		var views={
-        /*=========================
-          Model
-          ===========================*/
-        initialize:function(){
-            /*DOM*/
-            //注入至此DIV
-            this.divList=document.getElementById("ID-List");
+		var view={
+            /*=========================
+              Model
+              ===========================*/
+            initialize:function(){
+                /*DOM*/
+                this.divList=document.getElementById("ID-List");//注入至此DIV
 
-            /*Plugin*/
+                /*Data*/
+                this._initData();
+                
+                /*Plugin*/
+            },
+            /*=========================
+              Method
+              ===========================*/
+            _initData:function(){
+            	var self=this;
+            	//dummydata
+            	$.get("js/dummydata/data.json",function(data){
+            		var datalist=JSON.parse(data);
+            		//编译渲染
+            		var render = template.compile(tplIndex);
+            		var html = render({'children':datalist});
+            		self.divList.innerHTML=html;
+            		//测试Model
+            		console.log(validate.isEqual(1,1));
+            	});
+            	/*//realdata
+            	$.ajax({
+            		url:url,
+            		type:"get",
+            		dataType:"json",
+            		success:function(data){
+            			//编译渲染
+	            		var render = template.compile(tplIndex);
+	            		var html = render({'children':data});
+	            		self.divList.innerHTML=html;
+            		},
+            		error:function(msg){
+            			alert("连接服务器失败，请检查您的网络");
+            		}
+            	});*/
+            },
+            /*=========================
+              Plugin
+              ===========================*/
+            _initPlugin:function(){
 
-            /*Data*/
-            this._initData();
-        },
-        /*=========================
-          Method
-          ===========================*/
-        _initData:function(){
-        	var self=this;
-        	$.get("js/dummydata/data.json",function(data){
-  					var datalist=JSON.parse(data);
-  					//编译渲染
-  					var render = template.compile(tplIndex);
-  					var html = render({'children':datalist});
-  					self.divList.innerHTML=html;
-
-  					//测试Model
-  					console.log(validate.isEqual(1,1));
-  				});
-        },
-        /*=========================
-          Plugin
-          ===========================*/
-        _initPlugin:function(){
-
-        },
-        /*=========================
-          Events
-          ===========================*/
-        _attach:function(e){
-            var self=this;
-        },
-        /*=========================
-          Event Handler
-          ===========================*/
-    }
-    views.initialize();
+            },
+            /*=========================
+              Events
+              ===========================*/
+            _attach:function(e){
+                var self=this;
+            },
+            /*=========================
+              Event Handler
+              ===========================*/
+        }
+        view.initialize();
 	}
 );
